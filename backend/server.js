dfd8b9c1e4c1aba76819f1316b2051265e32b3bf
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-18 16:04:09 +0500

    code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./src/app");
+const dotenv = require('dotenv').config()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3200;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
