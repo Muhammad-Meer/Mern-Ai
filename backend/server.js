b766d646776a2cf3327be62e976daaffb0406b5c
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-19 12:58:42 +0500

    newcodee

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
 const app = require("./src/app");
 const dotenv = require('dotenv').config()
+const connectDb = require('./src/config/connectDb')
+
+
+const dns = require("dns");
+
+console.log("Before:", dns.getServers());
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+console.log("After:", dns.getServers());
+
+dns.resolve4("google.com", (err, addresses) => {
+  console.log(err, addresses);
+});
+
+
+connectDb()
 
 const PORT = process.env.PORT || 3200;
 
--- backend/src/config/connectDb.js
+++ backend/src/config/connectDb.js
@@ -1,5 +1,15 @@
-const  mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDb  = () => {
-    
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("connected")
+
+    } catch (error) {
+        console.log(error)
+
+    }
 }
+
+module.exports = connectDb
+
